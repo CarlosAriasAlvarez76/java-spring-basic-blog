2b09860418983e996bf1220d6f109dd2a11ec4e4
Arias Alvarez Carlos (EXT Aubay) <carlos.arias.alvarez@enel.com>
2022-09-14 09:51:47 +0200

    Model push 2

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,27 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class BlogController {
 
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap){
-        modelMap.put("title", "Blog Post 1");
+        List<Post> postList = this.postRepository.getAllPosts();
+        modelMap.put("posts", postList);
+        //modelMap.put("title", "Blog Post 1");
         return "home";
+    }
+
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
     }
 }
